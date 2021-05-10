6d3b0731ae0f9680fec14fd134680508dbe3edb5
CarlosPintoVacacela <c.pintov@uniandes.edu.co>
2021-05-09 22:33:43 -0500

    Pruebas cypress tags

---
+++ cypress-tests/cypress/page-objects/ghost-tags.js
@@ -0,0 +1,42 @@
+import { LoremIpsum } from "lorem-ipsum";
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
+
+lorem.generateWords(1);
+lorem.generateSentences(5);
+lorem.generateParagraphs(7);
+
+export class GhostTag {
+    login() {
+        cy.visit('ghost/#/signin')
+        cy.get('#ember8').type(Cypress.env('email'))
+        cy.get('#ember10').type(Cypress.env('pass'))
+        cy.get('#login').submit()
+    }
+
+    goTags() {
+        cy.visit('ghost/#/tags')
+    }
+
+    selectTag() {
+        cy.get('ol.tags-list',{ timeout: 10000 }).find('li.gh-tags-list-item > a').first().click({ force: true })
+    }
+    
+    selectInternalTag() {
+        cy.get('div.gh-contentfilter').find('button.gh-btn > span').contains('Internal tags').click({ force: true })
+        cy.get('ol.tags-list',{ timeout: 10000 }).find('li.gh-tags-list-item > a').first().click({ force: true })
+    }
+   
+
+   
+
+}
